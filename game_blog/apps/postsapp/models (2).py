eb33930fe2a4507939690f273faecae51413a97e
Pavel <pavel-zh@inbox.ru>
2023-07-09 13:38:32 +0300

    feat: add hashing

---
+++ game_blog/apps/postsapp/models (2).py
@@ -0,0 +1,17 @@
+import datetime
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils import UUIDType
+
+from ..database import Base
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    uid = Column(UUIDType, primary_key=True)
+    created_date = Column(DateTime, default=datetime.datetime.utcnow)
+    is_active = Column(Boolean, default=True)
+    title = Column(String)
+    content = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.uid"))
+    owner = relationship("User", back_populates="post")
---
+++ game_blog/core/config.py
@@ -0,0 +1,14 @@
+import os
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings
+
+SECRET_KEY = b"SECRET_KEY"
+if not SECRET_KEY:
+    SECRET_KEY = os.urandom(32)
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./site.db'
+ALEMBIC_SQLALCHEMY_DATABASE_URL = 'sqlite:///./site.db'
+
+templates = Jinja2Templates(directory="templates")
+
+TemplateResponse = templates.TemplateResponse
